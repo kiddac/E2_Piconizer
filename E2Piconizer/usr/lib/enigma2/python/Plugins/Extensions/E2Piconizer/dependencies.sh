99c418655fe6ed1aaa1048efe3c0abe34dcb7908
kiddac <kiddac2015@gmail.com>
2020-08-16 18:18:31 +0100

    no message

--- E2Piconizer/usr/lib/enigma2/python/Plugins/Extensions/E2Piconizer/dependencies.sh
+++ E2Piconizer/usr/lib/enigma2/python/Plugins/Extensions/E2Piconizer/dependencies.sh
@@ -3,25 +3,55 @@ pyv="$(python -V 2>&1)"
 echo "$pyv"
 echo "Checking Dependencies"
 echo
-if [[ $pyv =~ "Python 3" ]]; then
-	echo "checking python3-image"
-	opkg install python3-image
+
+if [ -d /etc/opkg ]; then
+	echo "updating feeds"
+	opkg update
 	echo
-	echo "checking python3-imaging"
-	opkg install python3-imaging
-	echo
-	echo "checking python3-multiprocessing"
-	opkg install python3-multiprocessing
-	echo
+	if [[ $pyv =~ "Python 3" ]]; then
+		echo "checking python3-image"
+		opkg install python3-image
+		echo
+		echo "checking python3-imaging"
+		opkg install python3-imaging
+		echo
+		echo "checking python3-multiprocessing"
+		opkg install python3-multiprocessing
+		echo
+	else
+		echo "checking python-image"
+		opkg install python-image
+		echo
+		echo "checking python-imaging"
+		opkg install python-imaging
+		echo
+		echo "checking python-multiprocessing"
+		opkg install python-multiprocessing
+		echo
+	fi
 else
-	echo "checking python-image"
-	opkg install python-image
-	echo
-	echo "checking python-imaging"
-	opkg install python-imaging
-	echo
-	echo "checking python-multiprocessing"
-	opkg install python-multiprocessing
-	echo
+    echo "updating feeds"
+    apt-get update
+    echo
+	if [[ $pyv =~ "Python 3" ]]; then
+		echo "checking python3-image"
+		apt-get install python3-image
+		echo
+		echo "checking python3-imaging"
+		apt-get install python3-imaging
+		echo
+		echo "checking python3-multiprocessing"
+		apt-get install python3-multiprocessing
+		echo
+	else
+		echo "checking python-image"
+		apt-get install python-image
+		echo
+		echo "checking python-imaging"
+		apt-get install python-imaging
+		echo
+		echo "checking python-multiprocessing"
+		apt-get install python-multiprocessing
+		echo
 fi
 exit 0
